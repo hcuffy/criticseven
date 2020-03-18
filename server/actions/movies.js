9f746d42221f7a0ace64045ce946db96e21bdd54
Henry Cuffy <hcuffy_5@hotmail.com>
2020-03-18 20:12:47 +0100

    get movies

--- server/actions/movies.js
+++ server/actions/movies.js
@@ -1,3 +1,14 @@
-export const getMovies = (request, response) => {
-  response.json({ name: 'Inception', genre: 'Thriller' })
+import { TmdbMovie } from '../tmdbWrapper'
+import axios from 'axios'
+
+const Tmdb = new TmdbMovie(process.env.API_KEY)
+
+export const getMovies = async (request, response) => {
+  try {
+    const movies = await Tmdb.getHomeMovies()
+    response.send(movies)
+    return
+  } catch (error) {
+    console.error(error)
+  }
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { getMovies } from '../actions'
 const router = express.Router()
 
-//router.get('/', getMovies)
-router.get('/movies', getMovies)
+router.get('/', getMovies)
+//router.get('/movies', getMovies)
 
 export default router

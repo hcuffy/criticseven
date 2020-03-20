848cdce11de46d671ff26e3d1a05a4250ae849a7
Henry Cuffy <hcuffy_5@hotmail.com>
2020-03-20 21:50:35 +0100

    added eslint

--- server/actions/movies.js
+++ server/actions/movies.js
@@ -1,14 +1,15 @@
-import { TmdbMovie } from '../tmdbWrapper'
-import axios from 'axios'
+import {TmdbMovie} from '../tmdbWrapper'
 
 const Tmdb = new TmdbMovie(process.env.API_KEY)
 
-export const getMovies = async (request, response) => {
-  try {
-    const movies = await Tmdb.getHomeMovies()
-    response.send(movies)
-    return
-  } catch (error) {
-    console.error(error)
-  }
+export const getMovies = async(request, response) => {
+	try {
+		const movies = await Tmdb.getHomeMovies()
+
+		response.send(movies)
+
+		return
+	} catch (error) {
+		console.error(error)
+	}
 }
--- server/tmdbWrapper/movieDBWrapper.js
+++ server/tmdbWrapper/movieDBWrapper.js
@@ -3,20 +3,20 @@ import axios from 'axios'
 const apiBaseURL = 'https://api.themoviedb.org/3'
 
 export default class TmdbMovie {
-  constructor(apiKey, language = 'en-US') {
-    this.apiKey = apiKey
-    this.language = language
-  }
+	constructor(apiKey, language = 'en-US') {
+		this.apiKey = apiKey
+		this.language = language
+	}
 
-  async getHomeMovies() {
-    try {
-      const { data } = await axios.get(
-        `${apiBaseURL}/discover/movie?api_key=${this.apiKey}&sort_by=popularity.desc`
-      )
+	async getHomeMovies() {
+		try {
+			const { data } = await axios.get(
+				`${apiBaseURL}/discover/movie?api_key=${this.apiKey}&sort_by=popularity.desc`
+			)
 
-      return data
-    } catch (error) {
-      console.error(error)
-    }
-  }
+			return data
+		} catch (error) {
+			console.error(error)
+		}
+	}
 }

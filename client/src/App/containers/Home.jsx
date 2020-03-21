b64c4cfe0d7ddcb8f23446e2b3ab55bb823054e0
Henry Cuffy <hcuffy_5@hotmail.com>
2020-03-21 15:17:46 +0100

    added eslint to the client

---
+++ client/src/App/containers/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {actionCreators} from '../actions/index'
+
+const Movies = ({moviesData, actions}) => {
+	return (
+  <div className="main_div">
+    <h4>HOME</h4>
+  </div>
+	)
+}
+
+const mapStateToProps = state => ({
+	moviesData: state.moviesData
+})
+
+const mapDispatchToProps = dispatch => ({
+	actions: bindActionCreators(actionCreators, dispatch)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Movies)
